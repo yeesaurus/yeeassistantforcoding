d31e6b14522ceda0f7cd8afb5dad63f004464c35
Adamus9 <76060398+Adamus9@users.noreply.github.com>
2021-01-08 21:04:56 +0100

    script.js

--- script.js
+++ script.js
@@ -5,14 +5,10 @@ function realtimeClock() {
     var hours = rtClock.getHours();
     var minutes = rtClock.getMinutes();
 
-    var amPm = ( hours < 12 ) ? "AM" : "PM";
-
-    hours = (hours > 12) ? hours - 12 : hours;
-
     hours = ("0" + hours).slice(-2);
     minutes = ("0" + minutes).slice(-2);
 
     document.getElementById('clock').innerHTML =
-        hours + ":" + minutes + " " + amPm;
+        hours + ":" + minutes;
     var t = setTimeout(realtimeClock, 500);
 }
