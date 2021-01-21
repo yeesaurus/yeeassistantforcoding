f5bdeaba530e5ddb58b0883a73dfd7305aa76960
yeesaurus <cokolicokolicokoli@gmail.com>
2021-01-21 09:28:07 +0100

    3 links included

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ function dosearch()
     var txt = document.getElementById("search").value;
                 
     var stackoverflow = "https://stackoverflow.com/search?q=" + txt + "&s=717bc4c6-ce7b-46c8-bddf-6d97e9bdd2bf";
+    var wikipedia = "https://en.wikipedia.org/wiki/" + txt ;
+    var reddit = "https://www.reddit.com/search?q=" + txt + "&type=sr%2Cuser";
                 
     document.write(stackoverflow);
+    document.write(wikipedia);
+    document.write(reddit);
 }
