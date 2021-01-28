d9d27197572fac37dbaec5789d050d8665faccb6
yeesaurus <cokolicokolicokoli@gmail.com>
2021-01-28 09:50:12 +0100

    links.html startup

--- script.js
+++ script.js
@@ -19,11 +19,6 @@ function dosearch()
     var stackoverflow = "https://stackoverflow.com/search?q=" + txt + "&s=717bc4c6-ce7b-46c8-bddf-6d97e9bdd2bf";
     var wikipedia = "https://en.wikipedia.org/wiki/" + txt ;
     var reddit = "https://www.reddit.com/search?q=" + txt + "&type=sr%2Cuser";
-
-    document.getElementById('stackoverflow').innerHTML = stackoverflow
-    document.getElementById('wikipedia').innerHTML = wikipedia
-    document.getElementById('reddit').innerHTML = reddit
+    
+    window.open("/links.html");
 }
-//<ul>
-//    <li><a href="links.html"></a></li>
-//</ul>
