12255f2a5d33476fb5ad923f863f726c9a0f1ef0
Adamus9 <76060398+Adamus9@users.noreply.github.com>
2021-01-28 15:33:40 +0100

    script.js Ez

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function realtimeClock() {
 
     var rtClock = new Date();
@@ -16,9 +17,12 @@ function dosearch()
 {
     var txt = document.getElementById("search").value;
 
-    var stackoverflow = "https://stackoverflow.com/search?q=" + txt + "&s=717bc4c6-ce7b-46c8-bddf-6d97e9bdd2bf";
-    var wikipedia = "https://en.wikipedia.org/wiki/" + txt ;
-    var reddit = "https://www.reddit.com/search?q=" + txt + "&type=sr%2Cuser";
-    
+    document.getElementById("stackoverflow").href = "https://stackoverflow.com/search?q=" + txt + "&s=717bc4c6-ce7b-46c8-bddf-6d97e9bdd2bf";
+    document.getElementById("reddit").href = "https://www.reddit.com/search?q=" + txt + "&type=sr%2Cuser";
+    document.getElementById("wikipedia").href = "https://en.wikipedia.org/wiki/" + txt ;
+
     window.open("/links.html");
 }
+//<ul>
+//    <li><a href="links.html"></a></li>
+//</ul>
